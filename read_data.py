1fc498b51aabdc9d0e7c5f34bfa697bff8c24b48
Jiancheng Zeng <zeng.jia@northeastern.edu>
2021-11-22 10:33:51 -0500

    211122-add 2 row testing modification

--- read_data.py
+++ read_data.py
@@ -15,7 +15,7 @@ from numpy import array
 Select all the directories that contains data files
 ----------------------
 """
-filenames = ["../attach-txt-files/data/d20210720/h20210720_1300.dat", "../attach-txt-files/data/d20210720/h20210720_1400.dat", "../attach-txt-files/data/d20210720/h20210720_1500.dat", "../attach-txt-files/data/d20210720/h20210720_1600.dat", "../attach-txt-files/data/d20210720/h20210720_1700.dat", "../attach-txt-files/data/d20210720/h20210720_1800.dat", "../attach-txt-files/data/d20210720/h20210720_1900.dat", "../attach-txt-files/data/d20210720/h20210720_2000.dat", "../attach-txt-files/data/d20210720/h20210720_2100.dat", "../attach-txt-files/data/d20210720/h20210720_2200.dat", "../attach-txt-files/data/d20210720/h20210720_2300.dat"]
+filenames = ["/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0000.dat", "/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0100.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0200.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0300.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0400.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0500.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0600.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0700.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0800.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0900.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1000.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1100.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1200.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1300.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1400.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1500.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1600.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1700.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1800.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1900.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2000.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2100.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2200.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2300.dat",]
 
 
 """
@@ -25,7 +25,7 @@ Read temperature data and combine them in one list, then save it to one txt file
 data = []
 for file in filenames:
     data.extend(array(np.genfromtxt(file, skip_header=1, skip_footer=0, names=None, dtype=float, usecols = list(range(25,72)), delimiter=',')))
-np.savetxt("../attach-txt-files/output/read_data_20210720.txt", data, delimiter = " , ", fmt='%1.4g')
+np.savetxt("/Users/jiancheng/GAPS/GAPS_local/output/read_data_20211119.txt", data, delimiter = " , ", fmt='%1.4g')
 #print(type(data))  #check data type
 print("finish reading data!")
 
@@ -40,5 +40,5 @@ for file in filenames:
     #data = data.extend(data_list)
 
 #print(type(T)) #check time data type
-np.savetxt("../attach-txt-files/output/read_time_20210720.txt", T, delimiter = "\n", fmt = "%s")
+np.savetxt("/Users/jiancheng/GAPS/GAPS_local/output/read_time_20211119.txt", T, delimiter = "\n", fmt = "%s")
 print("finish reading time!")
