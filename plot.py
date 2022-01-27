a58537dd9ee0447fcdeed24a924babdec09a2c80
Eclipsedclaw <jianchengzengkid@sina.cn>
2022-01-27 12:18:50 -0500

    220127-change gaps thermal plot python code directory

--- plot.py
+++ plot.py
@@ -15,10 +15,10 @@ from matplotlib.font_manager import FontProperties
 Select which RTDs to plot and name them
 ---------------------
 """
-RTDn = [1, 13, 14, 15, 6, 28, 29, 30, 40, 42, 44, 43] #single channel plot
+RTDn = [1, 7, 48, 22, 13, 14, 15, 16] #single channel plot
 #RTDn = list(range(1,48))  #plot RTD numbers
 #RTDt = ["reservoir", "L1 Buttom", "L1 Middle", "L1 Top", "L6 Buttom", "L6 Middle", "L6 Top", "L12 Buttom", "L12 Middle", "L12 Top"]
-RTDt = ["reservoir","Radiator", "Radiator", "Radiator", "Loop6 bottom position tube temperature", "Radiator", "Radiator", "Radiator",  "coupler temperature","Al frame close end temperature", "Al frame middle end temperature", "Al frame far end temperature"]
+RTDt = ["Reservoir", "Frame1", "Frame2", "Frame3", "Radiator", "Radiator","Radiator","Radiator"]
 STDEV = []  #standard deviation
 n=0
 #C = ['black', 'springgreen', 'forestgreen', 'darkgreen', 'lightcoral', 'indianred', 'brown', 'royalblue', 'blue', 'navy']  #customize plot color
@@ -28,13 +28,13 @@ n=0
 Read data from total data file
 --------------------
 """
-data = np.genfromtxt("/Users/jiancheng/CloudDocuments/Documents/GitHub/attach-txt-files/output/read_data_20210720.txt", skip_footer=0, skip_header=0, names=None, delimiter = " , ")    #get temperature data
-T = np.genfromtxt("/Users/jiancheng/CloudDocuments/Documents/GitHub/attach-txt-files/output/read_time_20210720.txt", skip_footer=0, skip_header=0, names=None, delimiter = "\n")    #get time data
+data = np.genfromtxt("/home/jiancheng/GAPS/GAPS_local/output/read_data_20220125.txt", skip_footer=0, skip_header=0, names=None, delimiter = " , ")    #get temperature data
+T = np.genfromtxt("/home/jiancheng/GAPS/GAPS_local/output/read_time_20220125.txt", skip_footer=0, skip_header=0, names=None, delimiter = "\n")    #get time data
 
 
 End = len(data[:,0])    #identify end time
-StartTime = 6390  #choose start time
-EndTime = 30390  #choose end time
+StartTime = 0  #choose start time
+EndTime = 40000  #choose end time
 #print(EndTime)
 
 """
@@ -55,8 +55,8 @@ Make legends and axis
 """
 plt.xlabel('time/s')
 plt.ylabel('temperature/$^\circ C$')
-#plt.ylim([-60, -20])
-plt.title('LN2 run test')
+#plt.ylim([-130, 30])
+plt.title('LN2 run test Jan 25 2022')
 plt.grid(color='k', linestyle='--', linewidth=.1)
 plt.legend(loc='lower left')
 #plt.annotate("start reservoir heater", [0,0])
--- read_data.py
+++ read_data.py
@@ -15,8 +15,9 @@ from numpy import array
 Select all the directories that contains data files
 ----------------------
 """
-filenames = ["/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0000.dat", "/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0100.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0200.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0300.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0400.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0500.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0600.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0700.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0800.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_0900.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1000.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1100.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1200.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1300.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1400.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1500.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1600.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1700.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1800.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_1900.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2000.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2100.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2200.dat","/Users/jiancheng/GAPS/GAPS_local/data/d20211119/h20211119_2300.dat",]
+#filenames = ["/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0000.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0100.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0200.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0300.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0400.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0500.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0600.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0700.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0800.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0900.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1000.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1100.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1200.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1300.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1400.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1500.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1600.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1700.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1800.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1900.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_2000.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_2100.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_2200.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_2300.dat"]
 
+filenames = ["/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0800.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_0900.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1000.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1100.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1200.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1300.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1400.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1500.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1600.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1700.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1800.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_1900.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_2000.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_2100.dat", "/home/jiancheng/GAPS/GAPS_local/data/d20220125/h20220125_2200.dat"]
 
 """
 Read temperature data and combine them in one list, then save it to one txt file
@@ -24,8 +25,8 @@ Read temperature data and combine them in one list, then save it to one txt file
 """
 data = []
 for file in filenames:
-    data.extend(array(np.genfromtxt(file, skip_header=1, skip_footer=0, names=None, dtype=float, usecols = list(range(25,72)), delimiter=',')))
-np.savetxt("/Users/jiancheng/GAPS/GAPS_local/output/read_data_20211119.txt", data, delimiter = " , ", fmt='%1.4g')
+    data.extend(array(np.genfromtxt(file, skip_header=1, skip_footer=0, names=None, dtype=float, usecols = list(range(25,73)), delimiter=',')))
+np.savetxt("/home/jiancheng/GAPS/GAPS_local/output/read_data_20220125.txt", data, delimiter = " , ", fmt='%1.4g')
 #print(type(data))  #check data type
 print("finish reading data!")
 
@@ -40,5 +41,5 @@ for file in filenames:
     #data = data.extend(data_list)
 
 #print(type(T)) #check time data type
-np.savetxt("/Users/jiancheng/GAPS/GAPS_local/output/read_time_20211119.txt", T, delimiter = "\n", fmt = "%s")
+np.savetxt("/home/jiancheng/GAPS/GAPS_local/output/read_time_20220125.txt", T, delimiter = "\n", fmt = "%s")
 print("finish reading time!")
